ddb93dac7450af36127be18a345ed75cb47acdbc
amin <aminfn4@gmail.com>
2025-04-17 16:25:06 +0330

    shopping page completed

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Home from "./Pages/Home/Home";
 import OrderHistory from "./Pages/OrderHistory/OrderHistory";
 import OtherProducts from "./Pages/OtherProducts/OtherProducts";
 import SelectedProduct from "./Pages/SelectedProduct/SelectedProduct";
+import Shopping from "./Pages/Shopping/Shopping";
 import UserProfile from "./Pages/UserProfile/UserProfile";
 import UserSignup from "./Pages/UserSignup/UserSignup";
 
@@ -14,6 +15,7 @@ const routes = [
     {path:"/account-info" , element :<AccountInfo/> },
     {path:"/other-products" , element :<OtherProducts/> },
     {path:"/selected-products" , element : <SelectedProduct/>},
+    {path:"/shopping" , element : <Shopping/>},
 
 ];
 export default routes;
